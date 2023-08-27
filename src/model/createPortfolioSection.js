9ff861ca0a2514751dcd3f91fa6482965c105b76
Mario <lazokmarios7@gmail.com>
2023-08-27 14:59:29 +0200

    add new projects

--- src/model/createPortfolioSection.js
+++ src/model/createPortfolioSection.js
@@ -2,14 +2,18 @@ import { createEl } from "../utils/createEl.js";
 
 export function createPortfolioSection() {
   const portfolioSection = createEl("section", {
-    className: "portfolio-section", id:"portfolio"
+    className: "portfolio-section",
+    id: "portfolio",
   });
   const portfolioTitle = createEl("p", {
     className: "portfolio-title",
     textContent: "[My latest work]",
   });
   const portfolioCard1 = createEl("div", { className: "portfolio-card" });
-  const portfolioPic1 = createEl("img", { className: "portfolio-pic" , src:"./media/webshop.png"});
+  const portfolioPic1 = createEl("img", {
+    className: "portfolio-pic",
+    src: "./media/webshop.png",
+  });
   const portfolioName1 = createEl("p", {
     className: "portfolio-name",
     textContent: "Webshop App",
@@ -29,7 +33,10 @@ export function createPortfolioSection() {
   portfolioSection.append(portfolioTitle, portfolioCard1);
 
   const portfolioCard2 = createEl("div", { className: "portfolio-card" });
-  const portfolioPic2 = createEl("img", { className: "portfolio-pic" , src:"./media/gallery.png"});
+  const portfolioPic2 = createEl("img", {
+    className: "portfolio-pic",
+    src: "./media/gallery.png",
+  });
   const portfolioName2 = createEl("p", {
     className: "portfolio-name",
     textContent: "Gallery App",
@@ -47,6 +54,52 @@ export function createPortfolioSection() {
 
   portfolioCard2.append(portfolioPic2, portfolioName2, portfolioDesc2, link2);
   portfolioSection.append(portfolioCard2);
+
+  const portfolioCard3 = createEl("div", { className: "portfolio-card" });
+  const portfolioPic3 = createEl("img", {
+    className: "portfolio-pic",
+    src: "./media/flivestar.png",
+  });
+  const portfolioName3 = createEl("p", {
+    className: "portfolio-name",
+    textContent: "Flivestar App",
+  });
+  const portfolioDesc3 = createEl("p", {
+    className: "portfolio-desc",
+    textContent:
+      "This project is greater than it looks at first glance. Frontend: VUE.js, backend: NODE.js, database: MySQL, mobile chat: Firebase, and so on.",
+  });
+  const link3 = createEl("a", {
+    href: "https://flivestar.com",
+  });
+  const goToButton3 = createEl("button", { textContent: "Go To App" });
+  link3.append(goToButton3);
+
+  portfolioCard3.append(portfolioPic3, portfolioName3, portfolioDesc3, link3);
+  portfolioSection.append(portfolioCard3);
+
+  const portfolioCard4 = createEl("div", { className: "portfolio-card" });
+  const portfolioPic4 = createEl("img", {
+    className: "portfolio-pic",
+    src: "./media/e-ticket.png",
+  });
+  const portfolioName4 = createEl("p", {
+    className: "portfolio-name",
+    textContent: "E-ticket system",
+  });
+  const portfolioDesc4 = createEl("p", {
+    className: "portfolio-desc",
+    textContent:
+      "You can create badges using template PDF files, edit and download all badges or tickets. Frontend & backend: SvelteKit, database: Edgedb.",
+  });
+  const link4 = createEl("a", {
+    href: "https://e-ticket.vip",
+  });
+  const goToButton4 = createEl("button", { textContent: "Go To App" });
+  link4.append(goToButton4);
+
+  portfolioCard4.append(portfolioPic4, portfolioName4, portfolioDesc4, link4);
+  portfolioSection.append(portfolioCard4);
 
   return portfolioSection;
 }
